32a4fad06e8cbe5c3643d9ef8cd9ba3e94898ac4
Loc <loc.tong@ptngloabalcorp.com>
2025-04-29 15:06:26 +0700

    update

--- t2q_template_pipeline.py
+++ t2q_template_pipeline.py
@@ -10,30 +10,34 @@ class BusinessExample(BaseModel):
     question: str
     answer: str
 
-class TranslateForm(BaseModel):
-    human_question: str
-    db_id: str
-    icl_type: str = ""
-    business_context: str = ""
-    business_rules: List[str] = []
-    business_examples: List[BusinessExample] = []
-    schema_description: str = ""
-class ExtractSchemaRequest(BaseModel):
+class SchemaAnnotation(BaseModel):
+    annotation: str
     database_url: str
-    schema_id: str
+    database_dialect: str
+
+class QueryRequest(BaseModel):
+    question: str
+    org_db_id: str
+    org_name: str
+    business_context: str | None = None
+    business_rules: List[str] | None = None
+    business_examples: List[BusinessExample] | None = None
+    schema_annotation: SchemaAnnotation | None = None
+
+class SchemaAnnotationRequest(BaseModel):
+    org_name: str
+    org_db_id: str
+    database_url: str
+    database_dialect: str = "postgresql"
 
 class Pipeline:
 
     class Valves(BaseModel):
-        DB_HOST: str
-        DB_PORT: str
-        DB_USER: str
-        DB_PASSWORD: str        
-        DB_DATABASE: str
+        ORG_NAME: str
+        DB_ID: str
+        DB_URL: str
+        DB_DIALECT: str = "postgresql"
         DB_SCHEMA_DESCRIPTION: str
-        # BUSINESS_CONTEXT: str
-        # BUSINESS_RULES: str
-        # EXAMPLE_LINK: str
 
     def __init__(self):
         # Optionally, you can set the id and name of the pipeline.
@@ -46,7 +50,6 @@ class Pipeline:
         self.T2Q_URL: str = os.getenv("T2Q_BASE_URL", "http://localhost:8000")
         self.T2Q_API_KEY: str = os.getenv("T2Q_API_KEY", "1234567890")
 
-        self.ICL_TYPE: str = os.getenv("ICL_TYPE", "zero_shot")
         self.OPENAI_MODEL: str = os.getenv("OPENAI_MODEL", "gpt-4o-mini")
         self.OPENAI_API_BASE_URL: str = os.getenv("OPENAI_API_BASE_URL", "https://api.openai.com/v1")
         self.OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY", "")
@@ -54,59 +57,77 @@ class Pipeline:
         self.valves = self.Valves(
             **{
                 # "pipelines": ["*"],   # Connect to all pipelines
-                # "name": "Organization PTN Employee Pipeline",                                                         
-                "DB_HOST": os.getenv("DB_HOST", "<<db_host>>"),
-                "DB_PORT": os.getenv("DB_PORT", "5432"),
-                "DB_USER": os.getenv("DB_USER", "<<db_user>>"),
-                "DB_PASSWORD": os.getenv("DB_PASSWORD", "<<db_password>>"),
-                "DB_DATABASE": os.getenv("DB_DATABASE", "<<db_database>>"),
+                # "name": "Organization PTN Employee Pipeline",
+                "ORG_NAME": os.getenv("ORG_NAME", "<<Organization Name>>"),
+                "DB_ID": os.getenv("DB_ID", "<<db_id>>"),
+                "DB_URL": os.getenv("DB_URL", "<<db_url>>"),
+                "DB_DIALECT": "postgresql",
                 "DB_SCHEMA_DESCRIPTION": os.getenv("DB_SCHEMA_DESCRIPTION", "<<to be updated>>")
-                # "BUSINESS_CONTEXT": "<<business_context>>",
-                # "BUSINESS_RULES": "<<business_rules>>",
-                # "EXAMPLE_LINK": "<<example_link>>",
             }
         )
 
         pass
 
     def init_db_connection(self):
-        self.engine = create_engine(f"postgresql://{self.valves.DB_USER}:{self.valves.DB_PASSWORD}@{self.valves.DB_HOST}:{self.valves.DB_PORT}/{self.valves.DB_DATABASE}")
+        print(f"init_db_connection:{__name__}")
+        print(f"🚀 DB_URL: {self.valves.DB_URL}")
+        self.engine = create_engine(f"{self.valves.DB_URL}")
         return self.engine
 
     async def on_startup(self):
-        self.init_db_connection()
-        # TODO: remove this after testing it should be loaded from the valves
+
+        print(f"on_startup:{__name__}")
         self.update_schema_description()
+
         pass
 
     async def on_valves_updated(self):
-        
-        self.init_db_connection()
+
+        print(f"on_valves_updated:{__name__}")
 
         self.update_schema_description()
 
     def update_schema_description(self):
-        if self.valves.DB_DATABASE != "<<db_database>>" and self.valves.DB_SCHEMA_DESCRIPTION == "<<to be updated>>":
+        
+        print(f"update_schema_description:{__name__}")
 
-            url = f"{self.T2Q_URL}/v1/t2q/extract-schema"
+        if self.valves.DB_URL != "<<db_url>>" and self.valves.DB_SCHEMA_DESCRIPTION == "<<to be updated>>" or self.valves.DB_SCHEMA_DESCRIPTION == "":
 
-            print(f"🚀 url: {url}")
+            self.init_db_connection()
 
-            request = ExtractSchemaRequest(
-                database_url= f"postgresql://{self.valves.DB_USER}:{self.valves.DB_PASSWORD}@{self.valves.DB_HOST}:{self.valves.DB_PORT}/{self.valves.DB_DATABASE}",
-                schema_id=self.valves.DB_DATABASE,
+            url = f"{self.T2Q_URL}/api/v1/schema_annotation/annotate"
+
+            print(f"🚀 Schema Annotation URL: {url}")
+
+            request = SchemaAnnotationRequest(
+                org_name=self.valves.ORG_NAME,
+                org_db_id=self.valves.DB_ID,
+                database_url=self.valves.DB_URL,
+                database_dialect=self.valves.DB_DIALECT,
+                ddl_file=""
             )
 
             try:
+                files = {'ddl_file': ('schema.sql', '')}
+
+                params = {
+                    'org_name': request.org_name,
+                    'org_db_id': request.org_db_id,
+                    'database_url': request.database_url,
+                    'database_dialect': request.database_dialect
+                }
+
                 r = requests.post(
-                    url=url,   
-                    headers={"X-API-Key": f"{self.T2Q_API_KEY}"},
-                    json=request.model_dump(),
+                    url=url,
+                    params=params,
+                    files=files
                 )
 
                 if r.status_code == 200:
+                    response_json = r.json()
+                    self.valves.DB_SCHEMA_DESCRIPTION = response_json["data"]["annotation"]
                     print(f"🚀 DB_SCHEMA_DESCRIPTION: {self.valves.DB_SCHEMA_DESCRIPTION}")
-                    self.valves.DB_SCHEMA_DESCRIPTION = str(r.json())
+
             except Exception as e:
                 print(f"🔥 Error: {e}")
                 print(f"🔥 Have to run the t2q composed service to get the schema description")
@@ -138,76 +159,137 @@ class Pipeline:
     def format_business_rules(self, business_rules: List[str]):
         # Rule 1: , Rule 2: , Rule 3:
         return ", ".join([f"Rule {i+1}: {rule}" for i, rule in enumerate(business_rules)])
+    
+    def call_openai_chat(self, messages: List[dict]):
+
+        headers = {}
+        headers["Authorization"] = f"Bearer {self.OPENAI_API_KEY}"
+        headers["Content-Type"] = "application/json"
+
+        payload = {
+            "model": self.OPENAI_MODEL,
+            "messages": messages,
+            "temperature": 0,
+            "max_tokens": 10
+        }
+
+        try:
+            r = requests.post(
+                url=f"{self.OPENAI_API_BASE_URL}/chat/completions",
+                json=payload,
+                headers=headers,
+            )
+
+            r.raise_for_status()
+            
+            response_content = r.json()["choices"][0]["message"]["content"].lower()
+            return response_content
+
+        except Exception as e:
+            print(f"Error calling OpenAI: {e}")
+            return ""
+        
+    def create_emoji_title(self, user_message: str):
+
+        print(f"🚀 user_message: {user_message}")
+
+        if "Create a concise" in user_message:
+
+            messages = [
+                {"role": "user", "content": user_message}
+            ]
+
+            return self.call_openai_chat(messages)
+        else:
+            return None
 
     def pipe(
         self, user_message: str, model_id: str, messages: List[dict], body: dict
     ) -> Union[str, Generator, Iterator]:
-        
-        print(f"🚀🚀🚀🚀🚀 body: {body}")
+                
+        print(f"pipe:{__name__}")
+
+        emoji_title = self.create_emoji_title(user_message)
+
+        if emoji_title:
+            print(f"🚀 emoji_title: {emoji_title}")
+            return emoji_title
+
 
         business_context = body.get("business_context", "")
         business_rules = body.get("business_rules", [])
-        examples = body.get("examples", [])
+        few_shot_examples = body.get("examples", [])
 
-        translate_form = TranslateForm(
-            human_question=user_message,
-            db_id=self.valves.DB_DATABASE,
-            icl_type=self.ICL_TYPE,
+        query_request = QueryRequest(
+            question=user_message,
+            org_db_id=self.valves.DB_ID,
+            org_name=self.valves.ORG_NAME,
             business_context=business_context,
             business_rules=business_rules,
-            business_examples=examples,
-            schema_description=self.valves.DB_SCHEMA_DESCRIPTION,
+            business_examples=[],
+            schema_annotation=SchemaAnnotation(
+                annotation=self.valves.DB_SCHEMA_DESCRIPTION,
+                database_url=self.valves.DB_URL,
+                database_dialect=self.valves.DB_DIALECT
+            )
         )
         
         business_intent_check = self.business_intent_check(
             user_message=user_message,
             business_context=business_context,
-            business_rules=self.format_business_rules(business_rules)
+            business_rules=business_rules
         )
 
-        print(f"🚀 business_intent_check: {business_intent_check}")
-        
         # Check if the question is business-related
-        if not business_intent_check:
-            return "I can only answer questions related to business and employee data. Please rephrase your question to focus on business-related topics."
+        if not business_intent_check:  
+            return "We can only answer questions related to related business. Or your question is too vague. Please rephrase your question to focus on business-related topics."
       
-        url = f"{self.T2Q_URL}/v1/t2q/translate"
+        url = f"{self.T2Q_URL}/api/v1/queries/generate"
 
         r = requests.post(
             url=url,   
-            headers={"X-API-Key": f"{self.T2Q_API_KEY}"},
-            json=translate_form.model_dump(),
+            # headers={"X-API-Key": f"{self.T2Q_API_KEY}"},
+            json=query_request.model_dump(),
         )
 
         if r.status_code == 200:
             try:
                 response = r.json()
 
-                predicted_sql_query = response["predicted_sql_query"]
+                generated_sql = response["generated_sql"]
 
-                if predicted_sql_query:
-                    
-                    query = f"```sql\n{predicted_sql_query}\n```"
-                    
-                    sql = text(predicted_sql_query)
-                    
+                print(f"🚀 generated_sql: {generated_sql}")
+
+                if generated_sql:
+
+                    query = f"```sql\n{generated_sql}\n```"
+
+                    self.init_db_connection()
+
                     with self.engine.connect() as connection:
-                        result = connection.execute(sql)
+                        result = connection.execute(text(generated_sql))
                         final_response = result.fetchall()
 
                     return f"**Generated SQL Query:**\n {query}\n\n\n**Data Response:**\n {self.format_markdown_results(final_response)}"
                 else:
-                    return "I wasn't able to translate that into SQL just yet - could you try rephrasing your question? 😊"
+                    return "I wasn't able to translate that into SQL just yet"
 
-            except requests.exceptions.JSONDecodeError as e:
+            except Exception as e:
                 print(f"🔥 Error: Received status code {r.status_code} and error: {e}")
-                return "I wasn't able to translate that into SQL just yet - could you try rephrasing your question? 😊"
-
+                if generated_sql:
+                    return f"This is the generated SQL query: \n {query}\n\n But I couldn't execute it. Please try again."
+                else:
+                    return "I wasn't able to translate that into SQL just yet - could you provide more context? 😊"
         else:
-            print(f"🔥 Error: Received status code {r.status_code} and error: {r.text}")
-            return "I wasn't able to translate that into SQL just yet - could you try rephrasing your question? 😊"
+            return "I wasn't able to translate that into SQL just yet - could you provide more context? 😊"
 
     def business_intent_check(self, user_message: str, business_context: str, business_rules: str):
+
+        if (business_context == "" and business_rules == []):
+            print(f"🚀 No business context or business rules provided")
+            return True
+        
+        business_rules = self.format_business_rules(business_rules)
 
         BUSINESS_INTENT_CHECK_PROMPT = """
             You are a business expert. You are given a question and business context (schema description, business context) and you need to determine if the question is business-related.
